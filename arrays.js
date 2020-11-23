1c705997c6f77fa5751be9a6a8b26ecce2b342d1
geldridge13 <73252369+geldridge13@users.noreply.github.com>
2020-11-23 14:14:10 -0500

    1,2

---
+++ arrays.js
@@ -0,0 +1,14 @@
+//let array=["one", "two", "three"]
+//if (array.length==0){
+    //alert ("this array is empty")
+//}
+//let s="default"
+
+//for (var i =0; i < array.length; i++){
+    //console.log(array[i])
+    //array[i]=s
+//}
+//console.log(array)
+
+let array=["one","two","three"];
+console.log(array.toString())
